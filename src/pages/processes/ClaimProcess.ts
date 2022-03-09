7538c0293515cabb8ab26cd619e11a62a529e041
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2022-03-09 19:51:07 +0100

    feat: added delegation process to the main process

--- src/pages/processes/ClaimProcess.ts
+++ src/pages/processes/ClaimProcess.ts
@@ -1,36 +1,32 @@
 import { Page } from 'puppeteer-core'
 import { Chain } from '../../models/Chain';
 import { ClaimRewards } from './ClaimReward';
+import { DelegateReward } from './DelegateReward';
 
 class ClaimProcess {
 
     private page: Page;
-    private chain?: Chain;
 
     constructor(page: Page) {
         this.page = page;
     }
-    
-    public setChain(chain: Chain) {
-        this.chain = chain;
-    }
 
-    public async start(): Promise<void> {
-        if (this.chain) {
-            const statusOk = await this.startFirstStep();
-            if (statusOk) {
-                this.startSecondStep();
-            }
+    public async start(chain: Chain): Promise<boolean> {
+        let statusOk = await this.startFirstStep(chain);
+        if (statusOk) {
+            statusOk = await this.startSecondStep(chain);
         }
+        return statusOk;
     }
 
-    private async startFirstStep(): Promise<boolean>  {
-        const claimProcess = new ClaimRewards(this.page, this.chain!)
+    private async startFirstStep(chain: Chain): Promise<boolean> {
+        const claimProcess = new ClaimRewards(this.page, chain)
         return claimProcess.start();
     }
 
-    private startSecondStep() {
-
+    private startSecondStep(chain: Chain) {
+        const delegateProcess = new DelegateReward(this.page);
+        return delegateProcess.start();
     }
 
 }
